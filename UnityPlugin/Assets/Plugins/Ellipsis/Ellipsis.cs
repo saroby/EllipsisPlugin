6c2efbbbebf12079b13db12f6a29d9fb6ca986b1
heewookshin <sin870320@gmail.com>
2020-02-21 14:59:30 +0900

    저장.

---
+++ UnityPlugin/Assets/Plugins/Ellipsis/Ellipsis.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class Ellipsis : MonoBehaviour
+{
+    public string GetTest()
+    {
+#if UNITY_EDITOR
+        return "unity editor";
+#elif UNITY_ANDROID
+        var jo = new AndroidJavaObject("com.dotdotdot.Ellipsis");
+        return jo.GetStatic<string>("getTest");
+#elif UNITY_IOS
+        return "TODO"; //TODO
+#endif
+        return null;
+    }
+}
+
+public partial class Ellipsis
+{
+    private static Ellipsis _instance;
+
+    public static Ellipsis Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = (Ellipsis) FindObjectOfType(typeof(Ellipsis));
+                if (_instance != null) return _instance;
+
+                var singletonObject = new GameObject();
+                _instance = singletonObject.AddComponent<Ellipsis>();
+                singletonObject.name = typeof(Ellipsis).ToString() + " (Singleton)";
+
+                DontDestroyOnLoad(singletonObject);
+            }
+
+            return _instance;
+        }
+    }
+}
---
+++ UnityPlugin/Assets/Scripts/EllipsisSample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EllipsisSample : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+        GUILayout.FlexibleSpace();
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+
+        GUILayout.Label(Ellipsis.Instance.GetTest());
+        
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+        GUILayout.FlexibleSpace();
+        GUILayout.EndArea();
+    }
+}
